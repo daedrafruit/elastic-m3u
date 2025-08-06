10532d89ecbd4e98764e1bd576baf3f502576960
daedrafruit <160567370+daedrafruit@users.noreply.github.com>
2025-08-05 20:47:13 -0700

    refactored greatly

--- elastic-m3u.py
+++ elastic-m3u.py
@@ -12,12 +12,11 @@ if matching metadata is found, replace the path:
 import os
 from tinytag import TinyTag
 from pathlib import Path
-from pathlib import Path
 from collections import defaultdict
 import re
 
-library = Path("Library/")
-m3u = "test.m3u"
+library = Path("Library2/")
+m3u_path = "test.m3u"
 audio_extentions = {".ogg" , ".mp3", ".acc", ".wav", ".flac", ".aiff"}
 
 metadata_path_cache = defaultdict(list)
@@ -42,54 +41,52 @@ def build_cache():
 def search_cache(albumartist, year, album, artist, title):
     return list(set(metadata_path_cache[albumartist]) & set(metadata_path_cache[year]) & set(metadata_path_cache[artist]) & set(metadata_path_cache[album]) & set(metadata_path_cache[title]))
 
-def extract_m3u_metadata(line):
+def get_metadata(line):
     pattern = r'(\w+)="([^"]*)"'
     matches = re.findall(pattern, line)
     return {key.lower(): value for key, value in matches}
 
-def get_comment_from_metadata(albumartist, year, album, artist, title):
+def get_comment(albumartist, year, album, artist, title):
     return str(f'# ALBUMARTIST="{albumartist}" YEAR="{year}" ARTIST="{artist}" ALBUM="{album}" TITLE="{title}"\n')
 
 def main():
-    f_in = open(m3u)
+    tmp_path = ".tmp_m3u"
+    if os.path.isfile(tmp_path): os.remove(tmp_path)
+    tmp = open(tmp_path, "x")
+    m3u = open(m3u_path)
 
-    lines = f_in.readlines()
-    if os.path.isfile("tmp_m3u"): os.remove("tmp_m3u")
-
-    f_out = open("tmp_m3u", "x")
     cache_built = False
 
     prevline = ""
+
+    lines = m3u.readlines()
     for line in lines:
+
         if line.startswith("#"):
             prevline = line
             continue
 
         path = line.rstrip('\n')
-        if not os.path.isfile(path):
-            tags = extract_m3u_metadata(prevline)
-
-            if not cache_built: 
-                build_cache()
-                cache_built = True
-
-            search_path = search_cache(tags["albumartist"], tags["year"], tags["album"], tags["artist"], tags["title"])
-            f_out.write(get_comment_from_metadata(tags["albumartist"], tags["year"], tags["artist"], tags["album"], tags["title"]))
-            f_out.write(str(search_path[0]) + "\n")
-
+        if os.path.isfile(path):
+            tag: TinyTag = TinyTag.get(path)
+            tmp.write(get_comment(tag.albumartist, tag.year, tag.artist, tag.album, tag.title))
+            tmp.write(line)
             prevline = line
             continue
 
-        tag: TinyTag = TinyTag.get(path)
-        f_out.write(get_comment_from_metadata(tag.albumartist, tag.year, tag.artist, tag.album, tag.title))
-        f_out.write(line)
+        tags = get_metadata(prevline)
+
+        if not cache_built: 
+            build_cache()
+            cache_built = True
+
+        matches = search_cache(tags["albumartist"], tags["year"], tags["album"], tags["artist"], tags["title"])
+        tmp.write(get_comment(tags["albumartist"], tags["year"], tags["artist"], tags["album"], tags["title"]))
+        tmp.write(str(matches[0]) + "\n")
+
         prevline = line
 
-    f_out = open("tmp_m3u", "r")
-    lines = f_out.readlines()
-    f_in = open(m3u, "w")
-    for line in lines:
-        f_in.write(line)
-    os.remove("tmp_m3u")
+    os.remove(m3u_path)
+    os.rename(tmp_path, m3u_path)
 
 main()
