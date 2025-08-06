6460320dc847bfbed1ab5dee59548f1cd3d73039
daedrafruit <160567370+daedrafruit@users.noreply.github.com>
2025-08-05 17:09:24 -0700

    can now read tags from m3u files and library

--- elastic-m3u.py
+++ elastic-m3u.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python
-
 '''
 open m3u file
 read each path
@@ -8,22 +6,42 @@ if a path is not broken, write/update a comment with its metadata
 if a path is broken, check if it has a metadata comment
 search the library for matching metadata
 
-if matching metadata is found, replace the path
+if matching metadata is found, replace the path:
 '''
 
-
 import os
+import glob
+from tinytag import TinyTag
+from pathlib import Path
+
+library = Path("Library/")
+audio_extentions = {".ogg"}
+
+paths = (path for path in library.glob(r'**/*') if path.suffix in audio_extentions)
+for path in paths:
+    print(path)
+    tag: TinyTag = TinyTag.get(path)
+    print(f"# ALBUMARTIST={tag.albumartist} YEAR={tag.year} ALBUM={tag.album} TITLE={tag.title}\n")
+
+
 
 f_in = open("test.m3u")
 
 lines = f_in.readlines()
-for line in lines:
-    print(line)
 
 f_out = open("out.m3u", "x")
+
 for line in lines:
-    if not line.startswith("# ALBUMARTIST="):
-        f_out.write(line)
+    if line.startswith("#"):
+        continue
+
+    path = line.rstrip('\n')
+    if not os.path.isfile(path):
+        continue
+
+    tag: TinyTag = TinyTag.get(path)
+    f_out.write(f"# ALBUMARTIST={tag.albumartist} YEAR={tag.year} ALBUM={tag.album} TITLE={tag.title}\n")
+    f_out.write(line)
 
 f_out = open("out.m3u", "r")
 lines = f_out.readlines()
@@ -31,3 +49,4 @@ for line in lines:
     print(line)
 
 os.remove("out.m3u")
+
